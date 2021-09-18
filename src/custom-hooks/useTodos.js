8ed569dee5c40227c8e6f23da2c2ee115c95aee7
autvincere <aut.vincere@gmail.com>
2021-09-18 18:35:52 -0300

    in progress section 2

--- src/custom-hooks/useTodos.js
+++ src/custom-hooks/useTodos.js
@@ -22,51 +22,11 @@ const useTodos = () => {
   };
 
   const completeTodo = (i) => {
-    // se hace necesaria la inclusion de un identificador por id dentro del objeto, para poder usar metodos que no muten el array
-    const selection = todos.find(item => )
-    console.log(selection);
-    //let value = todos[i]?.isCompleted
+    todos[i].isCompleted = !todos[i].isCompleted;
+    const selection = todos.splice(i, 1);
+    const newArr = [...todos, ...selection];
 
-    // if (!value) {
-    //   value = true;
-    //   const selection = todos.splice(i, 1)
-    //   //const changed = todos[i];
-    //  const newArr = [...todos, {text,value}];
-    //   setTodos(newArr);
-    //   console.log(todos[i]);
-    // } else {
-    //   value = false;
-    //   // todos[i].isCompleted = false;
-    //   // const changed = todos[i];
-    //   // const newArr = [...selection, ...todos];
-    //   // setTodos(newArr);
-    //   console.log(todos[i]);
-    // }
-
-    //todos[i].isCompleted = !todos[i].isCompleted;
-
-    // const selection = todos.splice(i, 1)
-    // const newArr = [...todos,...selection]
-
-    // if (todos[i].isCompleted) {
-    //   // todos[i].isCompleted = false;
-    //   // const selection = todos.splice(i, 1);
-    //   // const newArr = [...selection, ...todos];
-    //   // setTodos(newArr);
-    //   console.log('esta en true');
-    // } else {
-    //   console.log('esta en false');
-    // }
-
-    // if (todos[i].isCompleted === false) {
-    //   // todos[i].isCompleted = true;
-    //   // const selection = todos.splice(i, 1);
-    //   //  const newArr = [...todos, ...selection];
-    //   //  setTodos(newArr);
-    //   console.log('esta en false');
-    // }
-
-    // setTodos(newArr);
+    setTodos(newArr);
   };
 
   const removeTodo = (i) => {
